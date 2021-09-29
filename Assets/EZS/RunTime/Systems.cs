e7b4b8e2bce030fe5c8c72a8767a4f65284ffa7d
AlexWargon <wargonger@gmail.com>
2021-09-29 22:12:08 +0300

    ADD EZS

---
+++ Assets/EZS/RunTime/Systems.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Wargon.ezs
+{
+    public class Systems
+    {
+        public bool Alive;
+        private readonly GrowList<DestroySystem> destroySystemsList = new GrowList<DestroySystem>(4);
+        public int id;
+        private readonly GrowList<InitSystem> initSystemsList = new GrowList<InitSystem>(4);
+        private TypeMap<int, List<IOnAdd>> onAddSystems = new TypeMap<int, List<IOnAdd>>(4);
+        private TypeMap<int, List<IOnRemove>> onRemoveSystems = new TypeMap<int, List<IOnRemove>>(4);
+        public GrowList<UpdateSystem> updateSystemsList = new GrowList<UpdateSystem>(16);
+        private int updateSystemsCount;
+        private readonly World world;
+        private ISystemListener systemBigListener;
+        private bool debugMode;
+        public Systems(World world)
+        {
+            this.world = world;
+            world.AddSystems(this);
+        }
+
+        public void Init()
+        {
+            for (var i = 0; i < initSystemsList.Count; i++)
+                initSystemsList[i].Execute();
+            Alive = true;
+        }
+
+        public void SetListener(ISystemListener listener)
+        {
+            systemBigListener = listener;
+            debugMode = systemBigListener != null;
+        }
+
+        public Systems Add(UpdateSystem system)
+        {
+            updateSystemsList.Add(system);
+            system.Init(world.Entities, world);
+            InjectEntityTypesToSystem(system);
+            system.Update();
+
+            //Injector.InjectEntityType(system, world);
+            updateSystemsCount++;
+            return this;
+        }
+
+        public Systems Add(InitSystem system)
+        {
+            initSystemsList.Add(system);
+            system.Init(world.Entities, world);
+            return this;
+        }
+
+        public Systems Add(DestroySystem system)
+        {
+            destroySystemsList.Add(system);
+            system.Init(world.Entities, world);
+            return this;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public Systems AddReactive(IReactive system)
+        {
+            InjectEntityTypesToSystem(system);
+            if (system is IOnAdd add)
+            {
+                var triggerType = add.TriggerType;
+                if (!onAddSystems.HasKey(triggerType))
+                    onAddSystems.Add(triggerType, new List<IOnAdd>());
+                onAddSystems[triggerType].Add(add);
+                add.Init(world.Entities, world);
+                add.Execute();
+            }
+            else if (system is IOnRemove remove)
+            {
+                var triggerType = remove.TriggerType;
+                if (!onRemoveSystems.HasKey(triggerType))
+                    onRemoveSystems.Add(triggerType, new List<IOnRemove>());
+                onRemoveSystems[triggerType].Add(remove);
+                remove.Init(world.Entities, world);
+                remove.Execute();
+            }
+
+            return this;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal void OnAdd(int type)
+        {
+            if (!Alive) return;
+            if (!onAddSystems.HasKey(type)) return;
+            var cnt = onAddSystems[type].Count;
+            for (var index = 0; index < cnt; index++)
+            {
+                onAddSystems[type][index].Execute();
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal void OnRemove(int type)
+        {
+            if (!Alive) return;
+            if (!onRemoveSystems.HasKey(type)) return;
+            for (var index = 0; index < onRemoveSystems[type].Count; index++)
+            {
+                var system = onRemoveSystems[type][index];
+                system.Execute();
+            }
+        }
+
+        internal void Kill()
+        {
+            Alive = false;
+            for (var i = 0; i < destroySystemsList.Count; i++)
+                destroySystemsList[i].Execute();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void OnUpdate()
+        {
+            if (!Alive) return;
+            if (debugMode)
+            {
+                systemBigListener?.StartCheck();
+                for (var i = 0; i < updateSystemsCount; i++)
+                {
+                    systemBigListener?.StartCheck(i);
+                    updateSystemsList.Items[i].Update();
+                    systemBigListener?.StopCheck(i);
+                }
+                systemBigListener?.StopCheck();
+            }
+            else
+            {
+                for (var i = 0; i < updateSystemsCount; i++)
+                    updateSystemsList.Items[i].Update();
+            }
+        }
+
+        private void InjectEntityTypesToSystem(object system)
+        {
+            var systemType = system.GetType();
+            var entityType = typeof(EntityType);
+            var fields = systemType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            foreach (var f in fields)
+                if (f.FieldType.IsSubclassOf(entityType))
+                    f.SetValue(system, world.Entities.GetEntityType(f.FieldType));
+            
+        }
+    }
+
+    internal interface IOnAdd : IReactive
+    {
+        void Execute();
+    }
+
+    internal interface IOnRemove : IReactive
+    {
+        void Execute();
+    }
+
+    public interface IReactive
+    {
+        int TriggerType { get; }
+        void Init(Entities entities, World world);
+    }
+
+    public abstract class OnAdd<A> : IOnAdd
+    {
+        protected Entities entities;
+        protected World world;
+        public int TriggerType => ComponentType<A>.ID;
+
+        public void Init(Entities entities, World world)
+        {
+            this.entities = entities;
+            this.world = world;
+        }
+
+        public abstract void Execute();
+    }
+
+    public abstract class OnRemove<A> : IOnRemove
+    {
+        protected Entities entities;
+        protected World world;
+        public int TriggerType => ComponentType<A>.ID;
+
+        public void Init(Entities entities, World world)
+        {
+            this.entities = entities;
+            this.world = world;
+        }
+
+        public abstract void Execute();
+    }
+
+    public abstract class InitSystem
+    {
+        protected Entities entities;
+        protected World world;
+
+        public void Init(Entities entities, World world)
+        {
+            this.entities = entities;
+            this.world = world;
+        }
+
+        public abstract void Execute();
+    }
+
+    public abstract class DestroySystem
+    {
+        protected Entities entities;
+        protected World world;
+
+        public void Init(Entities entities, World world)
+        {
+            this.entities = entities;
+            this.world = world;
+        }
+
+        public abstract void Execute();
+    }
+
+    public abstract class UpdateSystem
+    {
+        protected Entities entities;
+        protected World world;
+
+        public virtual void Init(Entities entities, World world)
+        {
+            this.entities = entities;
+            this.world = world;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public abstract void Update();
+    }
+
+    public interface ISystemListener
+    {
+        void StartCheck();
+        void StopCheck();
+        void StartCheck(int index);
+        void StopCheck(int index);
+    }
+}
---
+++ Assets/EZS/Unity/Editor/MonoEntityEditor.cs
@@ -0,0 +1,192 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Object = System.Object;
+
+namespace Wargon.ezs.Unity {
+
+    [CustomEditor(typeof(MonoEntity), true), CanEditMultipleObjects]
+    public class MonoEntityEditor : Editor 
+    {
+        private MonoEntity monoEntity;
+        private MonoEntity[] manyEntities = new MonoEntity[16];
+        private bool flowed = true;
+        private GUIContent addComponentText;
+        private GUIStyle addComponentButtonStyle;
+        private Rect addButtonRect;
+        private bool editMany;
+        private int entitiesCount;
+        private void Awake() 
+        {
+            EntityGUI.Init();
+            ComponentTypesList.Init();
+        }
+
+        // private MonoEntity[] FindEntitiesWithSameComponents(Object[] targets)
+        // {
+        //     var entities = targets.Select(s1 =>(s1 as MonoEntity).Components).ToList().Intersect()
+        //     var mono = new MonoEntity[entitiesCount];
+        //     var index = 0;
+        //     var entityFromTargets1 = targets[0];
+        //     for (var i = 1; i < entitiesCount; i++)
+        //     {
+        //         var entityFromTargets1 = targets[i];
+        //         for (var j = 0; j < entitiesCount; j++)
+        //         {
+        //             var entityFromTargets2 = targets[j];
+        //             
+        //             
+        //         }
+        //     }
+        //
+        // }
+        public override void OnInspectorGUI()
+        {
+            //DrawDefaultInspector();
+            editMany = targets.Length > 1;
+            entitiesCount = targets.Length;
+            for (var i = 0; i < entitiesCount; i++)
+            {
+                manyEntities[i] = (MonoEntity) targets[i];
+            }
+            monoEntity = (MonoEntity)target;
+            
+            addComponentText = new GUIContent("Add Component");
+            addComponentButtonStyle = GUI.skin.button;
+            EditorGUI.BeginChangeCheck();
+
+            if (monoEntity.runTime)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.Space();
+                if (GUILayout.Button(new GUIContent("Kill Entity"),GUILayout.Width(154), GUILayout.Height(24)))
+                    monoEntity.Entity.Destroy();
+                EditorGUILayout.Space();
+                EditorGUILayout.EndHorizontal();
+            }
+            else
+            {
+                EditorGUILayout.BeginHorizontal();
+                monoEntity.destroyComponent = EditorGUILayout.Toggle("Destroy MonoBeh", monoEntity.destroyComponent);
+                monoEntity.destroyObject = EditorGUILayout.Toggle("Destroy GO", monoEntity.destroyObject);
+                EditorGUILayout.EndHorizontal();
+            }
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Run Time", monoEntity.runTime ? "✔" : "✘", EditorStyles.largeLabel);
+            EditorGUILayout.LabelField(monoEntity.runTime ? $"ID:{monoEntity.Entity.id.ToString()}" : "ID:X");
+            EditorGUILayout.EndHorizontal();
+            
+            
+            EntityGUI.Vertical(GUI.skin.box, () =>
+            {
+                if (monoEntity.runTime)
+                    if (monoEntity.Entity.IsDead())
+                    {
+                        EditorGUILayout.LabelField("ENTITY DEAD", EditorStyles.whiteLargeLabel);
+                        return;
+                    }
+                EntityGUI.Horizontal(() =>
+                {
+                    flowed = EditorGUILayout.Foldout(flowed, $"ECS Components [{monoEntity.ComponentsCount.ToString()}]");
+                    
+                    if (GUILayout.Button(new GUIContent("Clear", "Remove All Components")))
+                        RemoveAll();
+                });
+                if (flowed)
+                {
+                    addButtonRect = GUILayoutUtility.GetRect(addComponentText, addComponentButtonStyle);
+                    if (GUI.Button(addButtonRect, addComponentText, addComponentButtonStyle))
+                    {
+                        addButtonRect.y -= 20f;
+                        PopupWindow.Show(addButtonRect, new ComponentSearchPopup(AddComponent, manyEntities, entitiesCount));
+                    }
+                    
+                    DrawComponents();
+                }
+
+            });
+            EditorGUILayout.LabelField($"EZS", EditorStyles.whiteMiniLabel);
+
+            if (EditorGUI.EndChangeCheck())
+                EditorUtility.SetDirty(target);
+        }
+
+        
+        private object NewObject(Type type) {
+            return Activator.CreateInstance(type);
+        }
+
+        private Type GetComponentType(string typeName) {
+            //return Type.GetType(type + ",Assembly-CSharp", true);
+            return AppDomain.CurrentDomain.GetAssemblies()
+                .Select(x => Assembly.Load(x.FullName))
+                .Select(x => x.GetType(typeName))
+                .FirstOrDefault(x => x != null);
+            
+        }
+
+        private void RemoveAll() {
+            monoEntity.Components.Clear();
+        }
+
+        private void AddComponent(string componentName, MonoEntity entity) {
+            if (entity.runTime)
+                AddComponentRuntime(componentName,entity);
+            else
+                AddComponentEditor(componentName,entity);
+        }
+        
+        private void AddComponentEditor(string componentName, MonoEntity entity)
+        {
+            var type = GetComponentType(componentName);
+            if (entity.Components.HasType(type)) {
+                return;
+            }
+
+            var resolver = NewObject(type);
+            entity.Components.Add(resolver);
+        }
+        
+        private void AddComponentRuntime(string componentName, MonoEntity entity)
+        {
+            var type = GetComponentType(componentName);
+            if (entity.Entity.GetEntityData().componentTypes.Contains(ComponentTypeMap.GetID(type))) {
+                Debug.LogError($"ENTITY ALREADY HAS '{type}' COMPONENT");
+                return;
+            }
+            
+            var component= NewObject(type);
+            entity.Entity.AddBoxed(component);
+            entity.Components.Add(component);
+        }
+
+        private void DrawComponents() {
+            for (var index = 0; index < monoEntity.ComponentsCount; index++)
+                ComponentInspector.DrawComponentBox(monoEntity, index, manyEntities, entitiesCount);
+        }
+    }
+    public static class StringExtensions
+    {
+        public static bool Contains(this string source, string toCheck, StringComparison comp)
+        {
+            return source?.IndexOf(toCheck, comp) >= 0;
+        }
+    }
+    public static class ListExtension
+    {
+        public static bool HasType(this System.Collections.IList list, Type whatHas)
+        {
+            var i = 0;
+            var count = list.Count;
+            for (i = 0; i < count; i++)
+                if (list[i].GetType() == whatHas)
+                    return true;
+            return false;
+        }
+    }
+}
+
